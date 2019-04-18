924ede25519c97354c00994fc132f85b968f55f7
Adam Goode <adam-goode@hotmail.co.uk>
2019-04-18 22:36:25 +0100

    email amends

--- resources/views/emails/enquiry-internal.blade.php
+++ resources/views/emails/enquiry-internal.blade.php
@@ -2,14 +2,14 @@
 <head>
     <link rel="stylesheet" href="https://use.typekit.net/avc5yqy.css">
         <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400i|Poppins:400,500,600,700" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.typekit.net/ymr8req.css">
+        <link rel="stylesheet" href="https://use.typekit.net/sce2eaq.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
     <style>
         @import url("https://use.typekit.net/iqz8fbd.css");
         body {
             background: #e3e3e3;
-            font-family: "museo-sans", sans-serif;
+            font-family: futura-pt, sans-serif;
             margin: 0;
             padding: 0;
             color: #333333;
@@ -32,8 +32,8 @@
             -moz-box-shadow: 0px 0px 33px 0px rgba(0,0,0,0.21);
             box-shadow: 0px 0px 33px 0px rgba(0,0,0,0.21);
             transition: color ease 0.5s, background-color 0.5s ease, border 0.5s ease;
-            font-family: eurostile-extended, sans-serif;
-            font-weight: 500;
+            font-family: futura-pt, sans-serif;
+            font-weight: 700;
 
         }
         .btn:hover {
@@ -45,8 +45,8 @@
             color: #f00 !important;
         }
         .heading {
-            font-family: eurostile-extended, sans-serif;
-            font-weight: 500;
+            font-family: futura-pt, sans-serif;
+            font-weight: 700;
         }
     </style>
 </head>
@@ -55,21 +55,6 @@
             <table bgcolor="#e3e3e3" width="100%" border="0" cellspacing="0" cellpadding="0" style="background: #e3e3e3;">
                 <tr>
                     <td bgcolor="#e3e3e3" align="center" style="background: #e3e3e3;">
-
-                        <table width="600" style="background: #fff; background-size: cover; width: 600px; height: 201px; padding: 20px 0px; border-bottom: 3px solid #e3e3e3" border="0" cellspacing="0" cellpadding="0">
-                            <tr>
-                                <td>
-                                    <table align="left" width="100%" border="0" cellspacing="0" cellpadding="0">
-                                        <tr>
-                                            <td style="text-align: center; width: 120px;">
-                                                <a href=""><img class="logo" style="padding-top: 0px;" src="https://wildman.cherrytest.com/images/logo.svg" alt="Wildman Logo"></a>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </td>
-                            </tr>
-                        </table>
-
                         <table bgcolor="#fff" width="600" style="background: #fff; width: 600px; padding: 60px 50px;" border="0" cellspacing="0" cellpadding="0">
                             <tr>
                                 <td style="padding-bottom: 20px; text-align: left;">
@@ -78,7 +63,7 @@
                             </tr>
                             <tr>
                                 <td>
-                                    <p>You have had an enquiry through the website. Here are the details:</p>
+                                    <p>Someone has responded to Wedstock:</p>
                                     <p><b>Name: </b>{{$enquiry->names}}</p>
                                     <p><b>Is {{$enquiry->names}} attending? </b>{{$enquiry->attendance}}</p>
                                     <p><b>Enquiry:</b>{!! $enquiry->message !!}</p>
@@ -88,9 +73,9 @@
                         <table   width="600" style="background: #3f3f3f; border-top: 10px solid #3f3f3f; width: 600px; padding: 30px 50px; color: #ffffff;" border="0" cellspacing="0" cellpadding="0">
                             <tr>
                                 <td style="padding: 20px 0 10px;">
-                                    <div style="border-bottom: 1px solid #dadada; color: #ffffff; font-size: 18px; padding-bottom: 16px; font-family: eurostile-extended, sans-serif; margin-bottom: 16px;"><a style="text-decoration: none; color: white;" href="https://wildman.cherrytest.com/terms">Terms &amp; Conditions</a></div>
+                                    <div style="border-bottom: 1px solid #dadada; color: #ffffff; font-size: 18px; padding-bottom: 16px;    font-family: futura-pt, sans-serif;
+ margin-bottom: 16px;"><a style="text-decoration: none; color: white;" href="https://wildman.cherrytest.com/terms">Terms &amp; Conditions</a></div>
                                     <div style="font-size: 10px;">
-                                        <p>The information in this email is confidential, and may be legally privileged. It is intended solely for the addressee. Access to this e-mail by anyone else is unauthorised. If you are not the intended recipient, any disclosure, copying, distribution or any action taken or omitted to be taken in reliance on is prohibited and may be unlawful.</p>
                                     </div>
                                 </td>
                             </tr>
